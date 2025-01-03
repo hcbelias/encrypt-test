b0a27afc817739c109804c288fe9b574f119aa8e
Henrique Elias <hcbe2004@gmail.com>
2025-01-03 15:23:35 +0000

    Test

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,9 @@
-import { Outlet } from '@modern-js/runtime/router';
+import { Outlet } from "@modern-js/runtime/router";
 
 export default function Layout() {
   return (
     <div>
+      <h1>Encryption Test</h1>
       <Outlet />
     </div>
   );
